24a2e692d3438e53980f2f8b3a87335d7c095d8e
Serhii Yanchevskii <is@leogaming.net>
2023-07-22 17:29:38 +0300

    added http server

--- console/bin/httpServer.php
+++ console/bin/httpServer.php
@@ -4,29 +4,50 @@ declare(strict_types=1);
 
 require_once (__DIR__ . '/../../vendor/autoload.php');
 
-$server = new Swoole\HTTP\Server("0.0.0.0", 9501);
+use SW\Kernel;
 
-$server->set([
-    'worker_num' => 2,
-    'daemonize' => false,
-    'max_request' => 10000,
-    'dispatch_mode' => 2,
-    'debug_mode'=> 1,
-    'log_level' => 0,
-    'enable_coroutine' => true,
-    'open_http_protocol' => true,
-    'open_http2_protocol' => true,
-    'log_file' => '/var/log/supervisor/swoole/swoole_http_server.log',
-]);
+if (!defined('ENVAROMENT')) {
+    define('ENVAROMENT', 'dev');
+}
 
-$server->on("start", function (Swoole\Http\Server $server) {
-    echo "OpenSwoole http server is started at http://127.0.0.1:9501" . PHP_EOL;
+if (!isset($host)) {
+    $host = [
+        'host' => '0.0.0.0',
+        'port' => 9501
+    ];
+}
+if (!isset($config)) {
+    $config = [
+        'worker_num' => 1,
+        'daemonize' => false,
+        'max_request' => 10000,
+        'dispatch_mode' => 2,
+        'debug_mode'=> 0,
+        'log_level' => 3,
+        'enable_coroutine' => true,
+        'open_http_protocol' => true,
+        'open_http2_protocol' => true,
+        'log_file' => '/var/log/supervisor/swoole/swoole_http_server.log',
+    ];
+}
+
+$app = new Kernel();
+
+$server = new Swoole\HTTP\Server(host: $host['host'], port: $host['port']);
+
+$server->set($config);
+
+$server->on('start', function (Swoole\Http\Server $server) use ($host, $config, $app) {
+    echo 'Swoole http server is started at http://' . $host['host'] . ':' . $host['port'];
     echo PHP_EOL;
+
+//    $routes = $app->getContainerBuilder()->getParameter('route');
+//    var_dump($routes);
+//    echo PHP_EOL;
 });
 
-$server->on("request", function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
-    $response->header("Content-Type", "text/plain");
-    $response->end("Hello World\n");
+$server->on('request', function (Swoole\Http\Request $request, Swoole\Http\Response $response) use ($app) {
+    $app->action($request, $response)->end();
 });
 
 $server->start();
--- src/Kernel.php
+++ src/Kernel.php
@@ -3,7 +3,69 @@ declare(strict_types=1);
 
 namespace SW;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\TaggedContainerInterface;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
 class Kernel
 {
+    private TaggedContainerInterface $containerBuilder;
+    public function __construct(string|array $paths = [])
+    {
+        $this->generateCContainer($paths);
+    }
 
+    public function action(Request $request, Response $response): Response
+    {
+        $response->header("Content-Type", "text/plain");
+        $response->write("Hello World\n");
+
+        return $response;
+    }
+
+    public function getContainerBuilder(): TaggedContainerInterface
+    {
+        return $this->containerBuilder;
+    }
+
+    private function createRouts(): void
+    {
+        $routes = $this->getContainerBuilder()->getParameter('route');
+
+    }
+
+    private function generateCContainer(string|array $paths = []): void
+    {
+        if (!defined('BASE_APP_DIR')) {
+            define('BASE_APP_DIR', __DIR__ . '/..');
+        }
+        if (empty($paths)) {
+            $paths[] = __DIR__ . '/../config';
+            if (is_dir(BASE_APP_DIR . '/config')) {
+                $paths[] = BASE_APP_DIR . '/config';
+            }
+            if (is_dir(__DIR__ . '/config/' . ENVAROMENT)) {
+                $paths[] = __DIR__ . '/config/' . ENVAROMENT;
+            }
+        }
+
+        $this->containerBuilder = new ContainerBuilder();
+        $loader = new YamlFileLoader(
+            container: $this->containerBuilder,
+            locator: new FileLocator(paths: $paths)
+        );
+        foreach ($paths as $path) {
+            foreach (glob(rtrim($path, '/') . '/*.yaml') as $file) {
+                $fileName = pathinfo($file);
+                $loader->load($fileName['basename']);
+            }
+        }
+
+        $this->containerBuilder->compile();
+    }
 }
